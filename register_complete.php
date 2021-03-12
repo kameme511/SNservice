685d6ff5c0585e6573e1f5f509816f9a23fa9859
kameme511 <jagaimo0511@gmail.com>
2021-03-12 18:06:54 +0900

    Update register_complete.php

--- register_complete.php
+++ register_complete.php
@@ -1,4 +1,6 @@
 <?php
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
 /* ------------------------------
  * 必要なファイルを読み込む
  * ------------------------------ */
@@ -46,28 +48,26 @@ $statement->execute([
 unset($_SESSION['name']);
 unset($_SESSION['content']);
 unset($_SESSION['token']);
-
-setcookie('userName',$name, time()+60*60*24*14);
 ?>
 
 <!-- 描画するHTML -->
 <!doctype html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>登録成功</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>登録成功</title>
 </head>
 <body>
-    <header>
-        <h1>登録成功</h1>
-    </header>
-    <main>
-        <a href="index.html">戻る</a>
-    </main>
-    <footer>
-        <hr>
-    </footer>
+   <header>
+     <h1>登録成功</h1>
+   </header>
+   <main>
+      <a href="/">戻る</a>
+   </main>
+   <footer>
+       <hr>
+   </footer>
 </body>
 </html>
